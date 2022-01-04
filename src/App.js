fb0cf32c4a7692c5f7947f31be889a1785f203de
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-04 23:26:48 +0530

    Added links & Navigation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetups from './pages/AllMeetups';
 import NewMeetup from './pages/NewMeetup';
 import Favourites from './pages/Favourites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path='/' exact={true}>
           <AllMeetups />
