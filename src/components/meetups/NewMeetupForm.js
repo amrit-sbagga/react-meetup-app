8653b027bb8b2b59745f67ae8b6161f7defe879f
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-05 21:41:15 +0530

    added new meetup handler

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
-export default function NewMeetupForm() {
+export default function NewMeetupForm(props) {
     const titleInputRef = useRef();
     const imageInputRef = useRef();
     const addressInputRef = useRef();
@@ -23,7 +23,8 @@ export default function NewMeetupForm() {
             description : enteredDescription
         }
 
-        console.log(meetupData);
+        //console.log(meetupData);
+        props.onAddMeetup(meetupData);
     }
 
     return (
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,15 @@ import React from 'react'
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 export default function NewMeetup() {
+
+    function addMeetupHandler(meetupData){
+
+    }
+    
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </section>
     )
 }
