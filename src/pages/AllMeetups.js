cc5275bcdd9e3883f49fa6211154ab0ed2973f9b
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-07 01:11:44 +0530

    fetch all meetups data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_DATA = [
-    {
-      id: 'm1',
-      title: 'This is a first meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-      id: 'm2',
-      title: 'This is a second meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-];
+// const DUMMY_DATA = [
+//     {
+//       id: 'm1',
+//       title: 'This is a first meetup',
+//       image:
+//         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//       address: 'Meetupstreet 5, 12345 Meetup City',
+//       description:
+//         'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//     },
+//     {
+//       id: 'm2',
+//       title: 'This is a second meetup',
+//       image:
+//         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//       address: 'Meetupstreet 5, 12345 Meetup City',
+//       description:
+//         'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//     },
+// ];
 
 export default function AllMeetups() {
+
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ loadedMeetups, setLoadedMeetups ] = useState([]);
+
+    fetch(
+        'https://react-meetup-app-3f16e-default-rtdb.firebaseio.com/meetups.json'
+    ).then(response => {
+        return response.json();
+    }).then(data => {
+        setIsLoading(false);
+        setLoadedMeetups(data);
+    });
+
+    if(isLoading) {
+        return (
+          <section>
+            <p>Loading...</p>
+          </section>
+        )
+    }
+
     return (
         <section>
             <h1>All Meetups Page</h1>
@@ -33,7 +54,8 @@ export default function AllMeetups() {
                     })
                 }
             </ul> */}
-            <MeetupList meetups={DUMMY_DATA}/>
+            {/* <MeetupList meetups = { DUMMY_DATA }/> */}
+            <MeetupList meetups = { loadedMeetups }/>
         </section>
     )
 }
