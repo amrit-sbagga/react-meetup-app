96f3e6cf885da6a5328d1e1d7ea628a880c24f13
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-06 23:55:49 +0530

    Added fetch req

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -4,7 +4,7 @@ import NewMeetupForm from '../components/meetups/NewMeetupForm'
 export default function NewMeetup() {
 
     function addMeetupHandler(meetupData){
-
+        fetch('https://react-meetup-app-3f16e-default-rtdb.firebaseio.com/')
     }
     
     return (
